1e5e990bb4eae2b44050497a0ad100cbb9fcf101
RIKI KASHYAP <xonic.code@gmail.com>
2025-03-19 20:28:27 +0530

    Delete GSAP-Learning/Exercise/Animations-Basics directory

--- GSAP-Learning/Exercise/Animations-Basics/Practice-2/script.js
+++
@@ -1,8 +0,0 @@
-gsap.from("h1", {
-    color: "red",
-    opacity: 0,
-    duration: 2,
-    y: 50,
-    delay: 1,
-    stagger: 0.3
-})
--- GSAP-Learning/Exercise/Animations-Basics/Practice-3/script.js
+++
@@ -1,9 +0,0 @@
-gsap.to("#box", {
-    x: 1230,
-    y: 455,
-    duration: 2,
-    delay: 1,
-    rotate: 360,
-    repeat: 1,
-    yoyo: true
-})
