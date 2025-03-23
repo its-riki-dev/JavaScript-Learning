fce38cb5786b5c85a5a1f84221ffa8a8277fec8b
Riki Kashyap <xonic.code@gmail.com>
2025-03-23 19:18:41 +0530

    Added more DOM projects

---
+++ DOM-Practice/Q7/script.js
@@ -0,0 +1,29 @@
+// Build a Custom Cursor Animation with GSAP.
+
+var main = document.querySelector("#main");
+var cursor = document.querySelector("#cursor");
+var imageDiv = document.querySelector("#imageDiv");
+
+main.addEventListener("mousemove", function (dets) {
+    gsap.to(cursor, {
+        x: dets.x,
+        y: dets.y,
+        duration: 0.6,
+    });
+});
+
+imageDiv.addEventListener("mouseenter", function() {
+    cursor.innerHTML = "Click";
+    gsap.to(cursor, {
+        scale: 4,
+        backgroundColor: "#ffffff8a"
+    });
+});
+
+imageDiv.addEventListener("mouseleave", function() {
+    cursor.innerHTML = "";
+    gsap.to(cursor, {
+        scale: 1,
+        backgroundColor: "#fff"
+    });
+});
